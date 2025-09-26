c3c27a0f2b79515282b430be63c3c90178786a0d
abhaysingh-22 <abhaysingh6575@gmail.com>
2025-09-26 20:17:47 +0530

    feat: Add authentication routes and wrap application in AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -15,6 +16,8 @@ import Data from "./pages/Data";
 import News from "./pages/News";
 import Community from "./pages/Community";
 import Feedback from "./pages/Feedback";
+import Auth from "./pages/Auth";
+import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -25,26 +28,30 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <div className="min-h-screen flex flex-col">
-          <Navigation />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/impact" element={<Impact />} />
-              <Route path="/calculator" element={<Calculator />} />
-              <Route path="/destinations" element={<Destinations />} />
-              <Route path="/tips" element={<Tips />} />
-              <Route path="/data" element={<Data />} />
-              <Route path="/news" element={<News />} />
-              <Route path="/community" element={<Community />} />
-              <Route path="/feedback" element={<Feedback />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <AuthProvider>
+          <div className="min-h-screen flex flex-col">
+            <Navigation />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/impact" element={<Impact />} />
+                <Route path="/calculator" element={<Calculator />} />
+                <Route path="/destinations" element={<Destinations />} />
+                <Route path="/tips" element={<Tips />} />
+                <Route path="/data" element={<Data />} />
+                <Route path="/news" element={<News />} />
+                <Route path="/community" element={<Community />} />
+                <Route path="/feedback" element={<Feedback />} />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/profile" element={<Profile />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </AuthProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
