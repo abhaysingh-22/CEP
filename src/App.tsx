daffc45562da162b208fd33e60a0a53ab4318a0d
abhaysingh-22 <abhaysingh6575@gmail.com>
2025-09-27 01:27:38 +0530

    feat: Refactor App component to include Chatbot and Notes page

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+import { ThemeProvider } from "./contexts/ThemeProvider";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
+import Chatbot from "./components/Chatbot";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Impact from "./pages/Impact";
@@ -15,6 +17,7 @@ import Tips from "./pages/Tips";
 import Data from "./pages/Data";
 import News from "./pages/News";
 import Community from "./pages/Community";
+import Notes from "./pages/Notes";
 import Feedback from "./pages/Feedback";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
@@ -25,34 +28,38 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AuthProvider>
-          <div className="min-h-screen flex flex-col">
-            <Navigation />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/impact" element={<Impact />} />
-                <Route path="/calculator" element={<Calculator />} />
-                <Route path="/destinations" element={<Destinations />} />
-                <Route path="/tips" element={<Tips />} />
-                <Route path="/data" element={<Data />} />
-                <Route path="/news" element={<News />} />
-                <Route path="/community" element={<Community />} />
-                <Route path="/feedback" element={<Feedback />} />
-                <Route path="/auth" element={<Auth />} />
-                <Route path="/profile" element={<Profile />} />
-                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </AuthProvider>
-      </BrowserRouter>
+      <ThemeProvider defaultTheme="system" storageKey="ecotravel-theme">
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <AuthProvider>
+            <div className="min-h-screen flex flex-col">
+              <Navigation />
+              <main className="flex-grow">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/impact" element={<Impact />} />
+                  <Route path="/calculator" element={<Calculator />} />
+                  <Route path="/destinations" element={<Destinations />} />
+                  <Route path="/tips" element={<Tips />} />
+                  <Route path="/data" element={<Data />} />
+                  <Route path="/news" element={<News />} />
+                  <Route path="/community" element={<Community />} />
+                  <Route path="/notes" element={<Notes />} />
+                  <Route path="/feedback" element={<Feedback />} />
+                  <Route path="/auth" element={<Auth />} />
+                  <Route path="/profile" element={<Profile />} />
+                  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </main>
+              <Footer />
+              <Chatbot />
+            </div>
+          </AuthProvider>
+        </BrowserRouter>
+      </ThemeProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
